bf478230940c4ddac2c8895cd2b6a0d5d2efb1f8
jvopinho <jvopinho@isluny.org>
2025-12-04 18:25:34 -0300

    feat: enhance admin and content pages with new header and footer, improve theme toggle functionality

--- js/change-theme.js
+++ js/change-theme.js
@@ -1,20 +1,25 @@
+const button = document.getElementById('change-theme-button')
+
 localStorageSchema = localStorage.getItem('color-schema')
 if (localStorageSchema) {
     setColorSchema(localStorageSchema)
 }
 
-if (document.getElementById('change-theme-button')) {
-    const button = document.getElementById('change-theme-button')
+console.log(button);
 
+if (button) {
     button.onclick = function () {
         const body = document.querySelector('body')
         setColorSchema(body.getAttribute('data-colors-schema') == 'light' ? 'dark' : 'light')
         localStorage.setItem('color-schema', body.getAttribute('data-colors-schema'))
-
     }
 }
 
 function setColorSchema(schema) {
     const body = document.querySelector('body')
     body.setAttribute('data-colors-schema', schema)
+
+    if(button) {
+        button.innerText = schema == 'light' ? '🌙' : '☀️'
+    }
 }
